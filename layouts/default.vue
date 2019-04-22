b728507aa930a57436b5fbffdf64879b42682ec1
alastor <alastor141@gmail.com>
2019-04-22 11:52:55 +0300

    add state sections

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,9 @@
   <div>
     <log-out/>
     <login-form/>
-    <basket/>
-    <order/>
     <div>
       <nuxt-link active-class="active" to="/" exact>Главная</nuxt-link>
+      <nuxt-link active-class="active" :to="{ name: 'basket'}">Корзина</nuxt-link>
     </div>
     <div>
       <nuxt-link active-class="active" :to="{ name: 'section', params: { section: 'frames' }}">Оправы</nuxt-link>
@@ -17,17 +16,15 @@
 
 <script>
 
-import Basket from '~/components/basket/Basket.vue'
+
 import LogOut from '~/components/user/ButtonLogout.vue'
 import LoginForm from '~/components/user/LoginForm.vue'
-import Order from '~/components/order/Order.vue'
+
 
 export default {
   components: {
-    Basket,
     LoginForm,
     LogOut,
-    Order
   },
   computed: {
   },
---
+++ pages/order/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <basket/>
+    <order/>
+</div>
+</template>
+
+<script>
+
+import Order from '~/components/order/Order.vue'
+import Basket from '~/components/basket/Basket.vue'
+
+export default {
+    components: {
+        Order,
+        Basket 
+    },
+    head() {
+        return {
+            title: 'Оформление заказа'
+        }
+    }
+}
+</script>

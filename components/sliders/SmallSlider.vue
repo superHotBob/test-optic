9a405a19b7f768e6aa98b36cb4b0ad063933177f
SKITLZ <gskitlz@gmail.com>
2019-05-06 17:58:31 +0300

    index grid update

--- components/sliders/SmallSlider.vue
+++ components/sliders/SmallSlider.vue
@@ -1,47 +1,41 @@
 <template>
-<div>
+<!-- <div> -->
     <div class="small-slider" v-swiper:mySwiper="swiperOption">
         <div class="swiper-wrapper">
-            <nuxt-link class="swiper-slide" :class="{'s-date': date}" to="#0">
+            <nuxt-link class="swiper-slide banner-card" :class="{'s-date': date}" to="#0">
                 <img src="~/assets/images/placeholders/cards/03.jpg" alt="">
-                <div class="small-slider__over">
-                    <p>
-                        Скидки до 20% на элитные оправы Marc O'Polo
-                        <span v-if="date">
-                            <svg width="13" height="13"><use href="#svg-calendar"/></svg>
-                            03/08/2016
-                        </span>
-                    </p>
+                <div class="banner-card__over">
+                    <p>Скидки до 20% на элитные оправы Marc O'Polo</p>
+                    <span v-if="date">
+                        <svg width="13" height="13"><use href="#svg-calendar"/></svg>
+                        03/08/2016
+                    </span>
                 </div>
             </nuxt-link>
-            <nuxt-link class="swiper-slide" :class="{'s-date': date}" to="#0">
+            <nuxt-link class="swiper-slide banner-card" :class="{'s-date': date}" to="#0">
                 <img src="~/assets/images/placeholders/cards/04.jpg" alt="">
-                <div class="small-slider__over">
-                    <p>
-                        Лучшие цены на мировые линзы Essilor
-                        <span v-if="date">
-                            <svg width="13" height="13"><use href="#svg-calendar"/></svg>
-                            03/08/2016
-                        </span>
-                    </p>
+                <div class="banner-card__over">
+                    <p>Лучшие цены на мировые линзы Essilor</p>
+                    <span v-if="date">
+                        <svg width="13" height="13"><use href="#svg-calendar"/></svg>
+                        03/08/2016
+                    </span>
                 </div>
             </nuxt-link>
-            <nuxt-link class="swiper-slide" :class="{'s-date': date}" to="#0">
+            <nuxt-link class="swiper-slide banner-card" :class="{'s-date': date}" to="#0">
                 <img src="~/assets/images/placeholders/cards/05.jpg" alt="">
-                <div class="small-slider__over">
-                    <p>
-                        Изысканный дизайн оправ Brendel
-                        <span v-if="date">
-                            <svg width="13" height="13"><use href="#svg-calendar"/></svg>
-                            03/08/2016
-                        </span>
-                    </p>
+                <div class="banner-card__over">
+                    <p>Изысканный дизайн оправ Brendel</p>
+                    <span v-if="date">
+                        <svg width="13" height="13"><use href="#svg-calendar"/></svg>
+                        03/08/2016
+                    </span>
                 </div>
             </nuxt-link>
         </div>
         <div class="s-pagination main-container"></div>
     </div>
-</div>
+<!-- </div> -->
 </template>
 
 <script>

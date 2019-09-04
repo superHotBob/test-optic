b33592b6a85ab6230881e8ed7ea9874acff32bef
Alastor <alastor141@gmail.com>
2019-09-04 16:43:06 +0300

    fix tags catalog

--- components/catalog/Tags.vue
+++ components/catalog/Tags.vue
@@ -1,13 +1,25 @@
 <template>
-    <div class="tags">
+    <div class="tags" :class="{active:active}">
         <div class="tags__tag" v-for="(tag, index) in tags" :key="index">
             <nuxt-link :to="tag.URL" :class="{'active': $route.path == tag.URL}">{{tag.TITLE}}</nuxt-link>
         </div>
+        <span class="arrow" @click="dropdown()"></span>
     </div>    
 </template>
 
 <script>
 export default {
-    props: ['tags']
+    props: ['tags'],
+    data() {
+        return {
+            active:false
+        }
+    },
+    methods: {
+        dropdown() {
+            console.log('ok');
+            this.active = !this.active
+        }
+    }
 }
 </script>
